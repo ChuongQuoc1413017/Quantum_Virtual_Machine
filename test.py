0d3a55fe99d6963917ef0a7f6cc5f0b230f0135d
Nguyễn Quốc Chương <83415126+ChuongQuoc1413017@users.noreply.github.com>
2021-06-14 23:34:59 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ Created on Thu Apr 29 21:31:34 2021
 @author: ASUS
 """
 
-from QVM import qvm_circuit
-from QVM import qvm_gate
+from qCIT import qcit_circuit
+from qCIT import qcit_gate
 
 n = 2
-circuit = qvm_circuit.Qubit(n)
-qvm_gate.H(circuit, 0)
-qvm_gate.CNOT(circuit, 0, 1)
+circuit = qcit_circuit.Qubit(n)
+qcit_gate.H(circuit, 0)
+qcit_gate.CNOT(circuit, 0, 1)
 print(circuit.print_state())
 print()
 print(circuit.measure_sample(10000))
