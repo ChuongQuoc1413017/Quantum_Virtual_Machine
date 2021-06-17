041aba91f5610ca1dec8d8b46030e91719dafb02
Nguyễn Quốc Chương <83415126+ChuongQuoc1413017@users.noreply.github.com>
2021-06-17 17:26:59 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -5,14 +5,26 @@ Created on Thu Apr 29 21:31:34 2021
 @author: ASUS
 """
 
-from qCIT import qCircuit
-from qCIT import qBLAS
+from qSUN import qCircuit
+from qSUN import qGATES
+from qSUN import qMEAS
 
 n = 2
 circuit = qCircuit.Qubit(n)
-qBLAS.H(circuit, 0)
-qBLAS.CNOT(circuit, 0, 1)
+qGATES.H(circuit, 0)
+qGATES.CNOT(circuit, 0, 1)
+
+print('state:')
 print(circuit.print_state())
 print()
-print(circuit.measure_sample(10000))
+
+print('measure circuit:')
+print(qMEAS.measure_all(circuit, 10000))
+print()
+
+print('probabilities of qubit 1:')
+print(qMEAS.prob_qubit(circuit, 1))
+print()
+
+print('visualization:')
 print(circuit.visual_circuit())
