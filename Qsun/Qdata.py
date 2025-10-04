a9b71c07fe2c4d8ae7567c7699356a83ef2982ce
Quoc Chuong Nguyen <83415126+ChuongQuoc1413017@users.noreply.github.com>
2025-10-04 16:26:30 +0700

    Add files via upload

---
+++ Qsun/Qdata.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 11:30:23 2021
+
+@author: ASUS
+"""
+import numpy as np
+import math
+
+def intrinsic_dim_from_cov(X):
+    '''Return the estimated Intrinsic Dimension using the spectrum of the covariance matrix'''
+    cov = np.cov(X, rowvar=False)
+    eigvals = np.linalg.eigvalsh(cov)[::-1]
+    return ((eigvals.sum())**2) / ((eigvals**2).sum())
+
+def sepctral_complex_kernel(K, lambda_K = 0.1):
+    '''Return the Spectral Complexity of Kernel Matrix'''
+    return np.trace(K @ np.linalg.inv(K + lambda_K * np.identity(K.shape[0])))
+
+def kolmogorov_complex(X):
+    '''Return the estimated Kolmogorov complexity of a dataset'''
+    import zlib, gzip, lzma, bz2
+    
+    def serialize_array(X):
+        '''Return the raw data bytes of the dataset'''
+        # Simple deterministic serialization: shape + dtype + raw bytes
+        header = f"{X.shape}|{str(X.dtype)}|".encode('utf-8')
+        body = X.tobytes(order='C')
+        return header + body
+
+    def compressed_size_bytes(data_bytes, alg='gzip'):
+        '''Compress the data (bytes)'''
+        # data_bytes: bytes
+        if alg == 'zlib':
+            out = zlib.compress(data_bytes)
+        elif alg == 'gzip':
+            out = gzip.compress(data_bytes)
+        elif alg == 'lzma':
+            out = lzma.compress(data_bytes)
+        elif alg == 'bz2':
+            out = bz2.compress(data_bytes)
+        else:
+            raise ValueError("Unknown alg")
+        return len(out)
+    
+    b = serialize_array(X)
+    sizes = {}
+    algs=('zlib', 'gzip', 'lzma', 'bz2')
+    for a in algs:
+        sizes[a] = compressed_size_bytes(b, alg=a)
+    sizes['original_bytes'] = len(b)
+    sizes['best_bytes'] = min(sizes[a]/len(b) for a in algs)
+    return sizes
+
+
